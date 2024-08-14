013f35e7445200aa24f427ee72e2c2a42ee033f2
Joao Marcos <joao.marcos.witcel@gmail.com>
2024-08-13 21:31:25 -0300

    estruturando

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
 
 run:
 	g++ src/main.cpp -o target/main
-	./target/main
+	./target/main --scene teste.scene
--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,19 @@
 #include <stdio.h>
 #include <iostream>
 
-int main(void)
+int main(int argc, const char *argv[])
 {
-  std::cout << "Olá projeto novo!\n";
+  std::cout << "Iniciando...\n";
+
+  if (argc != 3)
+  {
+    std::cout << "Parâmetro --scene mal configurado ou faltando.\n";
+    return EXIT_FAILURE;
+  }
+
+  const char *scene_filename = argv[2];
+
+  std::cout << "Concluído...\n";
+  
   return EXIT_SUCCESS;
 }
