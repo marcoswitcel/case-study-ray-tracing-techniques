d889c73316d76aaa00ec3799bd041652f67a0c11
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-04-18 23:57:22 -0300

    WIP: pnm p3 e p6

--- src/ppm.cpp
+++ src/ppm.cpp
@@ -20,9 +20,11 @@ bool export_ppm_binary_file(const char *filename, const size_t width, const size
 bool export_portable_anymap_format(PNM_Type type, const char *filename, const size_t width, const size_t height, const uint8_t *buffer)
 {
   auto openmode = std::ios_base::out;
-  auto n_colors_per_pixel = 1;
-  if (type > P3) openmode |= std::ios_base::binary;
-  if (type == P3 || type == P6) n_colors_per_pixel = 3;
+  auto n_channels_per_pixel = 1;
+  bool is_binary = type > P3;
+
+  if (is_binary) openmode |= std::ios_base::binary;
+  if (type == P3 || type == P6) n_channels_per_pixel = 3;
 
   const char *magic_ident = magic_identifiers[type];
 
@@ -45,9 +47,20 @@ bool export_portable_anymap_format(PNM_Type type, const char *filename, const si
   // @todo João, implementar limite de caracteres por linha...
   // @todo João, ajustar para fazer em um write apenas
   // @todo João, avaliar e decidir como receber e escrever arquivos P1 e P4
-  for (size_t i = 0; i < (width * height * n_colors_per_pixel); i += 1)
+  if (is_binary)
+  for (size_t i = 0; i < (width * height * n_channels_per_pixel); i += 1)
   {
     ofs << buffer[i];
+  }
+  else
+  for (size_t i = 0, n_emmited = 0; i < (width * height * n_channels_per_pixel); i += 1, n_emmited += 1)
+  {
+    if (n_emmited == 3)
+    {
+      ofs << "\n";
+      n_emmited = 0;
+    }
+    ofs << std::to_string(buffer[i]) << " ";
   }
 
   ofs.close();
